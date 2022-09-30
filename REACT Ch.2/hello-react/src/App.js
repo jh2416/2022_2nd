99d820d48d6ffd9d8dedcc13c39c6724050e4301
jh2416 <wogns4576@gmail.com>
2022-09-30 17:00:54 +0900

    2.4.5

--- REACT Ch.2/hello-react/src/App.js
+++ REACT Ch.2/hello-react/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
-  const name = '뤼액트'; 
-    return <div>{name ==='리액트' && <h1>리액트입니다.</h1>}</div>
+  const name = undefined;
+  return <div>{name || '리액트'}</div>
 }
 
 export default App;
