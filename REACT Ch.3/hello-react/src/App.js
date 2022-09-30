4bde3348558273345afae4defeb8b12944c704b3
jh2416 <wogns4576@gmail.com>
2022-09-30 23:21:59 +0900

    react ch.3

---
+++ REACT Ch.3/hello-react/src/App.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
+}
+
+export default App;
