34a1a3ba9e861642670f271e3d047d63e4c6a5fc
jh2416 <wogns4576@gmail.com>
2022-09-30 22:59:59 +0900

    2.5

--- REACT Ch.2/hello-react/src/App.js
+++ REACT Ch.2/hello-react/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
-
 function App() {
   const name = '리액트';
   return (
-    <>
-      <div className="react">{name}</div>
-      <input /> 
-    </>
+    <div>
+      <div className="react"> {name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
   );
 }
 export default App;
