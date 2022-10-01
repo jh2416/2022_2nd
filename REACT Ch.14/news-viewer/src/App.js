6486e1429bd6dea999edbf0f9fa339796999c3f0
jh2416 <wogns4576@gmail.com>
2022-10-02 01:20:42 +0900

    14.4

--- REACT Ch.14/news-viewer/src/App.js
+++ REACT Ch.14/news-viewer/src/App.js
@@ -1,32 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d47442cb4ee242f1a30c3a8b9b5f5a05',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
--- REACT Ch.14/news-viewer/src/README.md
+++ REACT Ch.14/news-viewer/src/README.md
@@ -1,3 +1,5 @@
 ### 14.2 axios로 API호출해서 데이터 받아오기(async)
 
 ### 14.3 news API 불러오기
+
+### 14.4 news viewer UI만들기
