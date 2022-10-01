b4ec480c4e63a3f24f0638f7a94231b8d67adb17
jh2416 <wogns4576@gmail.com>
2022-10-01 15:49:58 +0900

    10.1

--- REACT Ch.9/styling-react/src/StyledComponent.js
+++ REACT Ch.9/styling-react/src/StyledComponent.js
@@ -1,14 +1,13 @@
-import styled, { css } from 'styled-components';
-
+import styled, { css } from "styled-components";
 
 const Box = styled.div`
-    /* props로 넣어준 값을 직접 전달해 줄 수 있습니다. */
-    background: ${props => props.color || 'blue'};
-    padding: 1rem;
-    display: flex;
+  /* props로 넣어준 값을 직접 전달해 줄 수 있습니다. */
+  background: ${(props) => props.color || "blue"};
+  padding: 1rem;
+  display: flex;
 `;
 
-const Button = styled.button
+const Button = styled.button`
   background: white;
   color: black;
   border-radius: 4px;
@@ -20,40 +19,33 @@ const Button = styled.button
   font-size: 1rem;
   font-weight: 600;
 
-
-
-/* & 문자를 사용하여 Sass처럼 자기 자신 선택 가능 */
+  /* & 문자를 사용하여 Sass처럼 자기 자신 선택 가능 */
   &:hover {
     background: rgba(255, 255, 255, 0.9);
   }
 
-
-
-/* 다음 코드는 inverted 값이 true일 때 특정 스타일을 부여해 줍니다. */
-  &{props =>
+  /* 다음 코드는 inverted 값이 true일 때 특정 스타일을 부여해 줍니다. */
+  ${(props) =>
     props.inverted &&
     css`
-        background: none;
-        border: 2px solid white;
-        color: white;
-        &:hover {
-            background: white;
-            color: black;
-        }
-    `};
-    & + button {
-        margin-left: 1rem;
+      background: none;
+      border: 2px solid white;
+      color: white;
+      &:hover {
+        background: white;
+        color: black;
       }
-    ;
+    `};
+  & + button {
+    margin-left: 1rem;
+  }
+`;
 
 const StyledComponent = () => (
-    <Box color="black">
-        <Button>안녕하세요</Button>
-        <Button inverted={true}>테두리만</Button>
-    </Box>
+  <Box color="black">
+    <Button>안녕하세요</Button>
+    <Button inverted={true}>테두리만</Button>
+  </Box>
 );
-      
-      
-      
-export default StyledComponent;
 
+export default StyledComponent;
