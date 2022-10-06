9d35dd2a43d4452d383c9e6e76581d57bb5e6874
jh2416 <wogns4576@gmail.com>
2022-10-06 21:11:20 +0900

    20.3.3

--- REACT Ch.20/ssr-recipe/config/webpack.config.server.js
+++ REACT Ch.20/ssr-recipe/config/webpack.config.server.js
@@ -10,7 +10,7 @@ const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
-const env = getClientEnvironment(paths.slice(0, -1));
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
 
 module.exports = {
   mode: "production", // 프로덕션 모드로 설정하여 최적화 옵션들을 활성화
