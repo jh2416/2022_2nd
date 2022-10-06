97c67bdf4b3bde916452c5a365a15bc747b5de8d
jh2416 <wogns4576@gmail.com>
2022-10-06 22:43:25 +0900

    20.4.7

--- REACT Ch.20/ssr-recipe/src/containers/UserContainer.js
+++ REACT Ch.20/ssr-recipe/src/containers/UserContainer.js
@@ -1,25 +1,24 @@
 import { useEffect } from "react";
-import Users from "../components/Users";
-import { useDispatch, useSelector } from "react-redux";
-import { getUsers } from "../modules/users";
+import User from "../components/Users";
+import { useSelector, useDispatch } from "react-redux";
+import { getUser } from "../modules/users";
 import { Preloader } from "../lib/PreloadContext";
 
-const UsersContainer = () => {
-  const users = useSelector((state) => state.users.users);
+const UserContainer = ({ id }) => {
+  const user = useSelector((state) => state.users.user);
   const dispatch = useDispatch();
 
-  // 컴포넌트가 마운트되고 나서 호출
   useEffect(() => {
-    if (users) return; // users가 이미 유효하다면 요청하지 않음
-    dispatch(getUsers());
-  }, [dispatch, users]);
+    if (user && user.id === parseInt(id, 10)) return; // 사용자가 존재하고, id가 일치한다면 요청하지 않음
+    dispatch(getUser(id));
+  }, [dispatch, id, user]);
 
-  return (
-    <>
-      <Users users={users} />
-      <Preloader resolve={() => dispatch(getUsers)} />
-    </>
-  );
+  // 컨테이너 유효성 검사 후 return null을 해야 하는 경우에
+  // null 대신 Preloader 반환
+  if (!user) {
+    return <Preloader resolve={() => dispatch(getUser(id))} />;
+  }
+  return <User user={user} />;
 };
 
-export default UsersContainer;
+export default UserContainer;
