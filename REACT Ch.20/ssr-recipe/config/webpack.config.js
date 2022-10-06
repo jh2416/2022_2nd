72202fe118998350534f1ec206d1c0d2388863ca
jh2416 <wogns4576@gmail.com>
2022-10-06 23:16:22 +0900

    20.5.2

--- REACT Ch.20/ssr-recipe/config/webpack.config.js
+++ REACT Ch.20/ssr-recipe/config/webpack.config.js
@@ -1,3 +1,4 @@
+const LoadablePlugin = require("@loadable/webpack-plugin");
 const fs = require("fs");
 const path = require("path");
 const webpack = require("webpack");
@@ -562,6 +563,7 @@ module.exports = function (webpackEnv) {
       ].filter(Boolean),
     },
     plugins: [
+      new LoadablePlugin(),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
