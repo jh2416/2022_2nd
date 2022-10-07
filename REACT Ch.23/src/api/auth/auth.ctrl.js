2df6931f65a148d27d3466645876457210526c72
jh2416 <wogns4576@gmail.com>
2022-10-07 18:09:01 +0900

    23.4.2

--- REACT Ch.23/src/api/auth/auth.ctrl.js
+++ REACT Ch.23/src/api/auth/auth.ctrl.js
@@ -37,6 +37,12 @@ export const register = async (ctx) => {
     await user.save(); // 데이터베이스에 저장
 
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -72,6 +78,11 @@ export const login = async (ctx) => {
       return;
     }
     ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
