accf281528d08137c4d637b60d65502992719f4b
jh2416 <wogns4576@gmail.com>
2022-10-09 00:02:12 +0900

    27.1.2

--- REACT Ch.24-27/blog/blog-frontend/src/containers/posts/PostViewerContainer.js
+++ REACT Ch.24-27/blog/blog-frontend/src/containers/posts/PostViewerContainer.js
@@ -4,15 +4,21 @@ import { withRouter } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
 import PostActionButtons from '../../components/post/PostActionButtons';
+import { setOriginalPost } from '../../modules/write';
+import { removePost } from '../../lib/api/posts';
+import { useParams, useNavigate } from 'react-router-dom';
+import user from '../../modules/user';
 
 const PostViewerContainer = ({ match }) => {
   // 처음 마운트될 때 포스트 읽기 API 요청
   const { postId } = match.params;
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { post, error, loading } = useSelector(({ post, loading }) => ({
     post: post.post,
     error: post.error,
     loading: loading['post/READ_POST'],
+    user: user.user,
   }));
 
   useEffect(() => {
@@ -23,12 +29,19 @@ const PostViewerContainer = ({ match }) => {
     };
   }, [dispatch, postId]);
 
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    navigate('/write');
+  };
+
+  const ownPost = (user && user._id) === (post && post.user._id);
+
   return (
     <PostViewer
       post={post}
       loading={loading}
       error={error}
-      actionButtons={<PostActionButtons />}
+      actionButtons={ownPost && <PostActionButtons onEdit={onEdit} />}
     />
   );
 };
